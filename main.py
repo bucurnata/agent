395f00d71e6fc359fb7e5b0cd93e25820ea4958a
bucurnata <bucur.nata20@gmail.com>
2025-06-18 19:42:27 +0300

    messases list and --verbose

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key=api_key)
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001',
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
-print(response.text)
-if hasattr(response, 'usage_metadata') and response.usage_metadata is not None:
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-else:
-    print("Usage metadata not available in the response.")
+def main():
+    load_dotenv()
+
+    args = sys.argv[1:]
+
+    if len(sys.argv) < 2:
+        print("Error: Prompt is not provided", file=sys.stderr)
+        sys.exit(1)
+
+    verbose = "--verbose" in args
+    if verbose:
+        args.remove("--verbose")
+
+    user_prompt = " ".join(args)
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=messages
+    )
+
+    if verbose and hasattr(response, 'usage_metadata') and response.usage_metadata is not None:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")    
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
